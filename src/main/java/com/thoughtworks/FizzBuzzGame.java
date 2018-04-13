6d8db5801c4b316ec8c2aa5a3a1be2d6e4a482a1
NicoleHaha <80515552@qq.com>
2018-04-13 14:11:51 +0800

    all

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -1,17 +1,68 @@
 package com.thoughtworks;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
 
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
-
+    Scanner scanner = new Scanner(System.in);
+    private int count;
+    public void setCount(){
+        System.out.println("请输入学生数：（回车键结束）");
+        scanner.nextInt();
+    }
     public void start(int count) {
-
+        this.count = count;
     }
 
+    public String isWhat(int num) {
+        int flag = 0;
+        String map = new String();
+        String temp = String.valueOf(num);
+        if (temp.contains("3")) {
+            return "Fizz";
+        }
+        if (num % 3 == 0) flag += 1;
+        if (num % 5 == 0) flag += 2;
+        if (num % 7 == 0) flag += 4;
+        switch (flag) {
+            case 0:
+                map = temp;
+                break;
+            case 1:
+                map = "Fizz";
+                break;
+            case 2:
+                map = "Buzz";
+                break;
+            case 3:
+                map = "FizzBuzz";
+                break;
+            case 4:
+                map = "Whizz";
+                break;
+            case 5:
+                map = "FizzWhizz";
+                break;
+            case 6:
+                map = "BuzzWhizz";
+                break;
+            case 7:
+                map = "FizzBuzzWhizz";
+                break;
+            default:
+                break;
+        }
+        return map;
+    }
     public List<String> getResults() {
+        IntStream.range(1,this.count+1).forEach(item->{
+            results.add(isWhat(item));
+        });
         return results;
     }
 }
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,56 @@
 package com.thoughtworks;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class FizzBuzzGameTest {
+    private FizzBuzzGame game;
+
+    @Before
+    public void setup() {
+        game = new FizzBuzzGame();
+    }
+
     @Test
     public void should_return_Fizz_when_have_3() {
+        assertThat(game.isWhat(3)).isEqualTo("Fizz");
+        assertThat(game.isWhat(13)).isEqualTo("Fizz");
+        assertThat(game.isWhat(35)).isEqualTo("Fizz");
+    }
 
+    @Test
+    public void should_return_Buzz_when_have_5() {
+        assertThat(game.isWhat(10)).isEqualTo("Buzz");
+        assertThat(game.isWhat(50)).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void should_return_Whizz_when_have_7() {
+        assertThat(game.isWhat(14)).isEqualTo("Whizz");
+        assertThat(game.isWhat(77)).isEqualTo("Whizz");
+    }
+
+    @Test
+    public void should_return_FizzBuzz_when_have_3and5() {
+        assertThat(game.isWhat(15)).isEqualTo("FizzBuzz");
+        assertThat(game.isWhat(45)).isEqualTo("FizzBuzz");
+    }
+
+    @Test
+    public void should_return_FizzWhizz_when_have_3and7() {
+        assertThat(game.isWhat(21)).isEqualTo("FizzWhizz");
+        assertThat(game.isWhat(84)).isEqualTo("FizzWhizz");
+    }
+
+    @Test
+    public void should_return_BuzzWhizz_when_have_5and7() {
+        assertThat(game.isWhat(70)).isEqualTo("BuzzWhizz");
+    }
+
+    @Test
+    public void should_return_FizzBuzzWhizz_when_have_357() {
+        assertThat(game.isWhat(105)).isEqualTo("FizzBuzzWhizz");
     }
 }
